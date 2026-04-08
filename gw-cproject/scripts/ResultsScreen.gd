3aa3e858d0087a11b1596640921f1e759a808284
LauryntheBean <lalabean07@icloud.com>
2026-04-07 21:18:32 -0400

    blah blah

--- gw-cproject/scripts/ResultsScreen.gd
+++ gw-cproject/scripts/ResultsScreen.gd
@@ -1,10 +1,13 @@
 #ResultsScreen.gd
+extends Control
+
 func _ready():
 	#This needs to be removed when we actually have a timer
 	GameData.completion_time = 95.0 
 	#Time can be changed for testing
 	
 	var personality = GameData.get_personality()
-	$personalityLabel.text = personality
-	draw_pie_chart(personality)
+	$PersonalityLabel.txt = personality
+	
+	
 	
