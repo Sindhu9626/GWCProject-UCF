7e220966d45be6c3b420a092d3bc6c9312a9a518
Alayna <alaynae06@gmail.com>
2026-04-13 20:58:44 -0400

    added notes text and small bug fix

--- continue_button.gd
+++ continue_button.gd
@@ -1,6 +1,5 @@
 extends Button
 
-func on_continue_pressed():
+func on_note_continue_pressed():
+	get_parent().visible = false
 	get_tree().change_scene_to_file("res://game_screen.tscn")
-	
-	owner.hide()
