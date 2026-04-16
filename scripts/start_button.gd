fd68d1218a986ee278a3b09c01cf44f3465cd5ac
sindhuja sesham <sindhuja.sesham13@gmail.com>
2026-04-16 15:52:18 -0400

    sorted files

--- scripts/start_button.gd
+++ scripts/start_button.gd
@@ -2,4 +2,4 @@ extends Button
 
 func _on_start_button_pressed():
 	print('Start button pressed\n')
-	get_tree().change_scene_to_file("res://notes_screen.tscn")
+	get_tree().change_scene_to_file("res://scences/notes_screen.tscn")
